1cce177673e603ab79aece0a383a841d3181b808
xiaoyao <635796359@qq.com>
2018-06-30 10:00:47 +0800

    add two api
    1:Specifies the key array lookup and return object
    2:Specifies the key value jump lookup and return accord first object

--- test/index.js
+++ test/index.js
@@ -3,13 +3,22 @@
 const utilJson = require('../index');
 var student = {
   "jhon":{
-    sex:"man",
-    age:19
+    "sex":'man',
+    "age":19,
+    "score":{
+      "sx":99,
+      "yw":88
+    }
   },
-  mary:{
-    sex:"woman",
-    age:18
+  "mary":{
+    "sex":"woman",
+    "age":18,
+    "score":{
+      "yy":150
+    }
   }
 };
-var who = utilJson.returnKeyForIndex(0,student);
-console.log(who);
+// var result = utilJson.returnKeyForIndex(0,student);
+// var result = utilJson.findObjectForKeys('sex',student);
+var result = utilJson.findObjectForKey('yy',student);
+console.log(result);
